2a55ba1ac86cc47d0acb46296dfc548586bf4dea
Montonen Sinja TTV15SP <sinjamontonen@kamk.fi>
2017-11-23 17:28:48 +0200

    shooting script added

---
+++ PipTheSecondComing/Assets/Scripts/TimedDestructor.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class TimedDestructor : MonoBehaviour
+{
+    public float time = 10f;
+
+    private float mTimer;
+
+    void Update()
+    {
+        mTimer += Time.deltaTime;
+        if (mTimer >= time)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ PipTheSecondComing/Assets/Scripts/Weapon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform bulletSpawn;
+    public GameObject bulletPrefab;
+
+    public Vector3 bulletDirection = Vector3.forward;
+    public float bulletSpeed = 10f;
+
+    //public float normalPitch = 1f;
+    //public float maxPitch = 1.25f;
+    //public float pitchCooldownTime = 1f;
+
+    //private AudioSource mAudio;
+
+    private float mLastShotTime = 0f;
+
+    void Start()
+    {
+        //mAudio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //if (Time.time - mLastShotTime >= pitchCooldownTime)
+            //{
+            //    mAudio.pitch = normalPitch;
+            //}
+            //else
+            //{
+            //    mAudio.pitch = normalPitch + (maxPitch - normalPitch) * (pitchCooldownTime - (Time.time - mLastShotTime));
+            //}
+
+            //mAudio.Play();
+
+            var bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation) as GameObject;
+            bullet.GetComponent<Rigidbody>().velocity = bulletDirection * bulletSpeed;
+
+            mLastShotTime = Time.time;
+        }
+    }
+}
