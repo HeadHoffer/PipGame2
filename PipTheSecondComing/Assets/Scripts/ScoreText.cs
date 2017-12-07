7421b47d99d4981107f77b8d291c1f9f652940e3
HeadHoffer <kim.1996hoffmann@gmail.com>
2017-12-07 16:46:08 +0200

    Added basic UI

---
+++ PipTheSecondComing/Assets/Scripts/ScoreText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject HPText;
+
+	void Start ()
+    {
+	}
+	
+	void Update ()
+    {
+        var text = GetComponent<Text>();
+        text.text = "Score: " + Player.GetComponent<SpaceShip>().PlayerScore;
+
+        var hpText = HPText.GetComponent<Text>();
+        hpText.text = "HP: " + Player.GetComponent<SpaceShip>().PlayerHP;
+    }
+}
